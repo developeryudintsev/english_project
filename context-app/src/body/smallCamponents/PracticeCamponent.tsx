1d870d2e38f77ca63f7cb7866a73593f3943a2f6
DilanBars2017 <developeryudintsev@gmail.com>
2025-08-23 04:10:28 +0300

    english_projectv0.7

--- context-app/src/body/smallCamponents/PracticeCamponent.tsx
+++ context-app/src/body/smallCamponents/PracticeCamponent.tsx
@@ -48,7 +48,7 @@ export const PracticeComponent: React.FC<PracticeComponentProps> = ({
 
     // const questions = data.simple[time][type];
     // const [currentQuestion, setCurrentQuestion] = useState(questions[0]);
-    const [fullData, setFullData] = useState<DataType | null>(null);
+    const [fullData, setFullData] = useState<DataType>(data);
     const [questions, setQuestions] = useState<QuestionType[]>([]);
     const [currentQuestion, setCurrentQuestion] = useState<QuestionType | null>(null);
     const [answerStatus, setAnswerStatus] = useState<"none" | "correct" | "wrong">("none");
